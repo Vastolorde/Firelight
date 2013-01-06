7c3827c6ff9f91ac2e66b839967054a7c0e463ef
Robin Malfait <robin@devrabb.it>
2013-01-06 07:41:19 +0100

    you can round buttons with a data-round attribute

--- js/firelight.js
+++ js/firelight.js
@@ -1,3 +1,13 @@
 $(function(){
-
+	// Boxes to get rounded
+	var boxes = $('.round');
+	$.each(boxes, function(){
+		var this_ = $(this);
+		var round = this_.data('round');
+		this_.css({
+			'-webkit-border-radius': round,
+			'-moz-border-radius': round, 
+			'border-radius': round
+		});
+	});
 });
